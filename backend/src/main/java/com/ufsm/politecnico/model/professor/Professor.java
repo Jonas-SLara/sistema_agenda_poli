0be993b4d313a5e581bebe68a57b4c347ac7f05f
Jonas <jonassilvadelara@gmail.com>
2025-09-03 16:35:17 -0300

    adicionado os atributos de entidades na camada de model

--- backend/src/main/java/com/ufsm/politecnico/model/professor/Professor.java
+++ backend/src/main/java/com/ufsm/politecnico/model/professor/Professor.java
@@ -2,18 +2,45 @@ package com.ufsm.politecnico.model.professor;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "professor")
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Professor{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    @Column(name = "matricula", nullable = false)
+    private String matricula;
+
+    @Override
+    public String toString(){
+        return new String(this.getNome() + this.getEmail()
+        + this.getMatricula() + this.getSenha() +
+        this.getId());
+    }
 }
--- backend/src/main/java/com/ufsm/politecnico/model/sala/Sala.java
+++ backend/src/main/java/com/ufsm/politecnico/model/sala/Sala.java
@@ -2,16 +2,44 @@ package com.ufsm.politecnico.model.sala;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "sala")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Sala {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "tipo", nullable = false)
+    private String tipo;
+
+    @Column(name = "predio", nullable = false)
+    private String predio;
+
+    @Column(name = "capacidade", nullable = false)
+    private int capacidade;
+
+    @Override
+    public String toString(){
+        return new String(this.getNome() +
+        this.getPredio() + this.getCapacidade() +
+        this.getTipo() + this.getId());
+    }
 }
